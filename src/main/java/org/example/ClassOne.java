4b5ed252f56bbadba9dd048b70fb9e32c56a42ce
Abhishek Chauhan <it1502713005@gmail.com>
2024-01-23 10:28:01 +0530

    Commit 2

--- src/main/java/org/example/ClassOne.java
+++ src/main/java/org/example/ClassOne.java
@@ -3,5 +3,6 @@ package org.example;
 public class ClassOne {
     public static void main(String[] args) {
         System.out.println("Class One");
+        System.out.println("Class Two commit");
     }
 }
